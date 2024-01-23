4db84d447f783a73a5663bbed1f8224b4738635b
Anees <30316990+VSBANEES@users.noreply.github.com>
2024-01-23 22:53:28 +0530

    Add files via upload

---
+++ minimum element.py
@@ -0,0 +1,7 @@
+sorted_list = [1, 3, 5, 7, 9, 11, 13]
+
+if sorted_list:
+    min_element = sorted_list[0]
+    print(f"The minimum element in the sorted list is: {min_element}")
+else:
+    print("The list is empty.")
